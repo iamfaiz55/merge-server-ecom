8f162278f02b9569dbf50080711060382c2f5d6d
shaikh faiz <138873480+iamfaiz55@users.noreply.github.com>
2024-12-14 20:05:38 +0530

    cors error 2

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static("public"))
 app.use(express.json())
 app.use(cookieparser())
 app.use(cors({
-    origin: "*",
+    origin: "http://localhost:8080/",
     credentials: true 
 }
 ))
